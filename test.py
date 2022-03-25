66c5e5e285e5c782f509c45182d9f87d6159a327
JoShMiQueL <joshmiquel.cs@gmail.com>
2022-03-25 03:40:42 +0100

    Update

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
 from __init__ import DarkSoulsRemastered
+from pymem import Pymem
+from offsets import offsets
+
+from utils import get_pointer
 
 # DarkSoulsRemastered().add_level(-1)
 DarkSoulsRemastered().start()
+
+# pm = Pymem("DarkSoulsRemastered")
+# module = pm.base_address
+
+# hp_ptr = get_pointer(pm, base + offsets["current_hp"][0], offsets["current_hp"][1])
+# hp_ptr = get_pointer(pm, base + offsets["current_hp"][0], offsets["current_hp"][1])
+# hp_ptr = get_pointer(pm, module + offsets["r_weapon_1"][0], offsets["r_weapon_1"][1])
+# hp = pm.read_int(hp_ptr)
+# print(hp_ptr)
+# print(hex(hp_ptr))
+# print(f"{hp_ptr:X}")
+# print(hp)
+# print(souls)
